2da7fffe70811aa7903d8a0a1d3043eeef483e3f
Ignas Bernotas <ignas@iber.lt>
2016-02-02 20:16:43 +0200

    Fixed question example files

--- examples/questions/multiple.choice.php
+++ examples/questions/multiple.choice.php
@@ -2,14 +2,13 @@
 
 include __DIR__ . '/../../vendor/autoload.php';
 
-$cursor = new \Iber\Console\UI\Cursor();
 $drawer = new \Iber\Console\UI\Drawer(
     new Symfony\Component\Console\Output\ConsoleOutput(),
     new \Iber\Console\UI\Window()
 );
 
 // allow multiple answers
-$question = new \Iber\Console\Question\MultipleChoiceQuestion($cursor, $drawer);
+$question = new \Iber\Console\Question\MultipleChoiceQuestion($drawer);
 $question->setChoices([
     'Yep', 'Nope', 'Maybe'
 ]);
--- examples/questions/single.choice.php
+++ examples/questions/single.choice.php
@@ -2,14 +2,13 @@
 
 include __DIR__ . '/../../vendor/autoload.php';
 
-$cursor = new \Iber\Console\UI\Cursor();
 $drawer = new \Iber\Console\UI\Drawer(
     new Symfony\Component\Console\Output\ConsoleOutput(),
     new \Iber\Console\UI\Window()
 );
 
 // allow only a single answer
-$question = new \Iber\Console\Question\Question($cursor, $drawer);
+$question = new \Iber\Console\Question\Question($drawer);
 $question->setChoices([
     'Yep', 'Nope', 'Maybe'
 ]);
